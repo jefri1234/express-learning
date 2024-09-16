2cc74377f799dbb53a15333e87fbaecc2e1d1096
max <obregonjefferson2@gmail.com>
2024-09-15 23:00:30 -0500

    añadiendo mas rutas con respuestas diferentes

--- http_response/index.js
+++ http_response/index.js
@@ -2,26 +2,56 @@
 const express= require('express')
 
 const app= express()
-
 app.listen(3000, function(){
     console.log("levantado en el puerto 3000")
 })
+
+//FORMAS EN QUE PUEDE RESPONDER EL SERVIDOR
+//1. RESPONDER CON UNA PAGINA HTML
+//2. RESPONDER EN FORMATO JSON
+//3. RESPONDER EN FORMATO XML
+//4. RESPONDER EN FORMATO PLAIN TEXT
+//5. RESPONDER CON UN ARCHIVO
+//6. RESPONDER CON UNA IMAGEN
+//7. RESPONDER CON UN VIDEO
+//8. RESPONDER CON UN AUDIO
+//10. RESPONDER CON UN ARCHIVO PDF
+//11. RESPONDER CON UN ARCHIVO CSV
+//12. RESPONDER CON UN ARCHIVO EXCEL
+//13. RESPONDER CON UN ARCHIVO WORD
+//14. RESPONDER CON UN ARCHIVO POWERPOINT
 
 app.get('/',function(req,res){
     res.send("Hola mundo")
 })
 
-app.get('/archivo',function(req,res){
+app.get('/imagen',function(req,res){
     res.sendFile('./boy.jpeg',{
         root: __dirname
     })
 })
 
-//enviar codigo de ESTADO
+app.get('/word',function(req,res){
+    res.download('./word.docx',{
+        root: __dirname
+        })
+})
+app.get('/pdf',(req,res)=>{
+    res.sendFile('./jeff.pdf',{
+        root: __dirname
+        })
 
-//404=> no encontre lo que intentas eliminar
-//500=> error interno del servidor
-//200=> todo bien
+})
+
+
+
+// SERVIDOR enviar codigo de ESTADO
+//200 OK                        =>todo ok
+//404 NOT FOUND
+//500 INTERNAL SERVER ERROR     => error interno del servidor
+//201 CREATED
+//400 BAD REQUEST               => solicitud incorrecta
+//201 CREATED
 //204=> todo bien pero no devolvo nada de contenido solo (respuesta estado) 
 
 
