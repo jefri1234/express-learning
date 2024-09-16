d86e326ce3e704126397266da6de824f5045e6ad
max <obregonjefferson2@gmail.com>
2024-09-15 22:45:27 -0500

    add middlware y express pueda entender datos que envia el cliente

--- reques_body/index.js
+++ reques_body/index.js
@@ -8,7 +8,16 @@ app.listen(3000,function(){
 
 //midlleware= permite que express reconosca los formatos JSON que le envia el cliente
 app.use(express.json())
+app.use(express.text())
 
+//para que express entienda los datos que vienen de un  formulario añado este middleware
+//especificar que es un url simple que no necesite interpretar nada complicado, solo dato texto😱
+app.use(express.urlencoded({extended:false}))
+
+//creacion ruta post 
+//del REQUEST realizo una extraccion del BODY 
+//obtengo los valores del body
+//con esa data puedo buscar, guardar, y muchas operaciones que yo quiera
 app.post('/crear',function(req,res){
     const {nombre,edad} = req.body
     const usuario = {nombre,edad}
