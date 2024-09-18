afcc132226dfb92aa66eef058159c56a4b83d13a
max <obregonjefferson2@gmail.com>
2024-09-17 21:17:18 -0500

    especificando funcionamiento metodo all

--- method_all/index.js
+++ method_all/index.js
@@ -7,3 +7,8 @@ app.listen(4000)
 app.all('/',function(req,res){
     res.send('Hello World')
 })
+//este metodo all lo que hace es lo siguiente= LA RUTA / tiene todos los verbos http 
+//get post put delete etc 
+//si no especificamos un metodo en la ruta, por defecto se ejecuta el metodo all
+//esto es muy util para las rutas que no necesitan de un metodo especifico
+//como por ejemplo la ruta raiz /
