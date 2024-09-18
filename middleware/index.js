e33fea88aeca1409821a9e5feabca7eaefa8cfcb
max <obregonjefferson2@gmail.com>
2024-09-18 13:23:41 -0500

    implementando un middleware basico

--- middleware/index.js
+++ middleware/index.js
@@ -4,3 +4,16 @@ const app = express()
 
 app.listen(3000)
 
+//creacion de un middleware
+
+//next => continua con tu flujo normal
+app.use(function(req,res,next){
+    console.log('middleware')
+    console.log("URL:"+req.url +"---- METODO:"+ req.method)
+    next()
+})
+
+
+app.get('/profile',function(req,res){
+    res.send('visit route profile')
+})
