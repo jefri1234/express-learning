4e14f2c9a0c29fca096fbed29ffd93be9646baf4
max <obregonjefferson2@gmail.com>
2024-09-19 23:39:58 -0500

    uso del middleware de morgan instalamos y usamos o tambien los que vienen de mismo express

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
-
+const morgan = require('morgan')//usams un middleware lo instalamos
 const app = express()
 
 app.listen(3000)
 
 //creacion de un middleware
+
+//instale un middleware y lo ejecuto
+//Devuelve imformacion como= fecha que se hizo la peticio
+//de que navegador se hizo , el metodo http y mas 
+//ASI HAY MAS MIDDLEWARE QUE PODEMOS USARLOS = validar datos,subir img,comprobar si user tiene valor en su peticion como cookie
+app.use(morgan())
+
+//middleware para que expres pueda reconocer estos formatos😐😱
+app.use(express.json())
+app.use(express.text())
 
 //next => continua con tu flujo normal
 app.use(function(req,res,next){
