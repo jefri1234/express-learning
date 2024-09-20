211046606eb0c6da0cb2fed0ee14cff604fe2270
max <obregonjefferson2@gmail.com>
2024-09-19 23:59:06 -0500

    create first route the rest api with middleware morgan

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan =require('morgan')
 
 const app = express()
 
 app.listen(3000,function(){
     console.log("sever on")
+})
+app.use(morgan('dev'))
+
+app.get('/',(req,res)=>{
+    res.send("ruta inicial")
 })
