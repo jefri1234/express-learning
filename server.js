8723f373bfefa4ba5c03ae50a4623ab53af828b2
max <obregonjefferson2@gmail.com>
2024-09-30 14:26:15 -0500

    update files

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ const morgan =require('morgan')
 const app = express()
 //GET ENVIRONMENT VARIABLE 
 const puerto= process.env.PORT
-//MIDDLEWARE
-app.use(morgan('dev'))
 
 app.listen(puerto,function(){
     console.log("sever on")
 })
-app.use(morgan('dev'))
 
-app.get('/',(req,res)=>{
+app.get('/jeff',(req,res)=>{
     res.send("ruta inicial")
 })
